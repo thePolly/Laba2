c11c0d10166e18e4657f74ce0aeef24e079fdff7
thePolly <lin997@yandex.ru>
2020-03-25 10:01:53 +0300

    1

--- src/main/java/ru/Main.java
+++ src/main/java/ru/Main.java
@@ -14,7 +14,6 @@ public class Main {
         List<Integer> nodes = new ArrayList<Integer>();
         int node;
 
-
             System.out.println("Type number of nodes = >");
             m = scanner.nextInt();
             for (int i = 0; i < m; i++) {
@@ -26,10 +25,7 @@ public class Main {
 
                 nodes.add(node);
             }
-
-
-
-        List<ChordNode> chordNodes = new ArrayList<ChordNode>();
+            List<ChordNode> chordNodes = new ArrayList<ChordNode>();
         for (int i = 0; i < nodes.size(); i++) {
             chordNodes.add(new ChordNode(nodes.get(i), m, nodes));
         }
@@ -39,19 +35,21 @@ public class Main {
             System.out.println(chordNode);
         });
 
-        chordNodes = addNode(6, nodes, m);
 
-        chordNodes.forEach(chordNode -> {
-            System.out.println("Node " + chordNode.getId());
-            System.out.println(chordNode);
-        });
-
-        chordNodes = deleteNode(6, nodes, m);
-
-        chordNodes.forEach(chordNode -> {
-            System.out.println("Node " + chordNode.getId());
-            System.out.println(chordNode);
-        });
+//
+//        chordNodes = addNode(6, nodes, m);
+//
+//        chordNodes.forEach(chordNode -> {
+//            System.out.println("Node " + chordNode.getId());
+//            System.out.println(chordNode);
+//        });
+//
+//        chordNodes = deleteNode(6, nodes, m);
+//
+//        chordNodes.forEach(chordNode -> {
+//            System.out.println("Node " + chordNode.getId());
+//            System.out.println(chordNode);
+//        });
 
 
         int tmp = 0;
@@ -69,12 +67,14 @@ public class Main {
                 case (2):
                     System.out.println("Type index =>");
                     tmp2 = scanner.nextInt();
-                    System.out.println(addNode(tmp2, nodes, m));
+                    chordNodes = addNode(tmp2, nodes, m);
+                    System.out.println(chordNodes);
                     break;
                 case (3):
                     System.out.println("Type index =>");
                     tmp2 = scanner.nextInt();
-                    System.out.println(deleteNode(tmp2,  nodes, m));
+                    chordNodes=deleteNode(tmp2,  nodes, m);
+                    System.out.println(chordNodes);
                     break;
                 case (4):
                     chordNodes.forEach(chordNode -> {
@@ -90,62 +90,7 @@ public class Main {
 
     }
 
-    private static void testMain(){
-        List<ChordNode> chordNodes = new ArrayList<>();
 
-        List<Integer> nodes = Arrays.asList(0, 1, 3);
-
-        List<Integer> ids = Arrays.asList(0, 1, 3);
-
-        List<Integer> successors = Arrays.asList(1, 3, 0);
-
-        List<Integer> predecessors = Arrays.asList(3, 0, 1);
-
-        List<List<Integer>> starts = Arrays.asList(Arrays.asList(1, 2, 4),
-                                                    Arrays.asList(2, 3, 5),
-                                                    Arrays.asList(4, 5, 7));
-
-        List<List<List<Integer>>> intervals = Arrays.asList(Arrays.asList(Arrays.asList(1, 2),
-                                                                            Arrays.asList(2, 4),
-                                                                            Arrays.asList(4, 0)),
-                                                            Arrays.asList(Arrays.asList(2, 3),
-                                                                            Arrays.asList(3, 5),
-                                                                            Arrays.asList(5, 1)),
-                                                            Arrays.asList(Arrays.asList(4, 5),
-                                                                            Arrays.asList(5, 7),
-                                                                            Arrays.asList(7, 3)));
-        List<List<Integer>> nodeList = Arrays.asList(Arrays.asList(1, 3, 0),
-                                                        Arrays.asList(3, 3, 0),
-                                                        Arrays.asList(0, 0, 0));
-
-        int m = 3;
-        for (int i = 0; i < nodes.size(); i++) {
-            chordNodes.add(new ChordNode(nodes.get(i), m, nodes));
-        }
-
-        for(int i = 0; i < chordNodes.size(); i++){
-            ChordNode node = chordNodes.get(i);
-            assert node.getId().equals(ids.get(i));
-            assert node.getPredecessor().equals(predecessors.get(i));
-            assert node.getSuccessor().equals(successors.get(i));
-
-            for(int j = 0; j < node.getFingerStarts().size(); j++){
-                FingerTable fingerTable = node.getFingerStarts().get(j);
-
-                assert fingerTable.getNode().equals(nodeList.get(i).get(j));
-                assert fingerTable.getStart().equals(starts.get(i).get(j));
-                assert fingerTable.getFrom().equals(intervals.get(i).get(j).get(0));
-                assert fingerTable.getTo().equals(intervals.get(i).get(j).get(1));
-            }
-
-        }
-
-        assert findSuccessor(2, 0, chordNodes, 3).equals(3);
-        for(int i = 4; i < 8; i++){
-            assert findSuccessor(i, 0, chordNodes, 3).equals(0);
-        }
-
-    }
 
     public static Integer findSuccessor(int id, int startNodeIndex, List<ChordNode> nodes, int m){
         Integer predecessor = findPredecessor(id, startNodeIndex, nodes, m);
@@ -155,11 +100,17 @@ public class Main {
         return result;
     }
 
+
     private static Integer findPredecessor(int id, int startNodeIndex, List<ChordNode> nodes, int m){
         ChordNode node = nodes.get(startNodeIndex);
         Integer intNode = node.getId();
         Integer successor = node.getSuccessor();
+        int loopCount = 0;
         while(notIn(id, intNode, successor, m)){
+            loopCount++;
+            if(loopCount > Math.pow(2, m)){
+                return nodes.get(0).getPredecessor();
+            }
             intNode = findClosest(id, intNode, nodes, m);
             final int finalIntNode = intNode;
             successor = nodes.stream().filter(e -> {
@@ -168,6 +119,7 @@ public class Main {
         }
         return intNode;
     }
+
 
     private static Integer findClosest(int id, int intNode, List<ChordNode> nodes, int m){
         ChordNode node = nodes.stream().filter(e -> {
@@ -223,5 +175,61 @@ public class Main {
         }
 
         return result;
+    }
+    private static void testMain(){
+        List<ChordNode> chordNodes = new ArrayList<>();
+
+        List<Integer> nodes = Arrays.asList(0, 1, 3);
+
+        List<Integer> ids = Arrays.asList(0, 1, 3);
+
+        List<Integer> successors = Arrays.asList(1, 3, 0);
+
+        List<Integer> predecessors = Arrays.asList(3, 0, 1);
+
+        List<List<Integer>> starts = Arrays.asList(Arrays.asList(1, 2, 4),
+                Arrays.asList(2, 3, 5),
+                Arrays.asList(4, 5, 7));
+
+        List<List<List<Integer>>> intervals = Arrays.asList(Arrays.asList(Arrays.asList(1, 2),
+                Arrays.asList(2, 4),
+                Arrays.asList(4, 0)),
+                Arrays.asList(Arrays.asList(2, 3),
+                        Arrays.asList(3, 5),
+                        Arrays.asList(5, 1)),
+                Arrays.asList(Arrays.asList(4, 5),
+                        Arrays.asList(5, 7),
+                        Arrays.asList(7, 3)));
+        List<List<Integer>> nodeList = Arrays.asList(Arrays.asList(1, 3, 0),
+                Arrays.asList(3, 3, 0),
+                Arrays.asList(0, 0, 0));
+
+        int m = 3;
+        for (int i = 0; i < nodes.size(); i++) {
+            chordNodes.add(new ChordNode(nodes.get(i), m, nodes));
+        }
+
+        for(int i = 0; i < chordNodes.size(); i++){
+            ChordNode node = chordNodes.get(i);
+            assert node.getId().equals(ids.get(i));
+            assert node.getPredecessor().equals(predecessors.get(i));
+            assert node.getSuccessor().equals(successors.get(i));
+
+            for(int j = 0; j < node.getFingerStarts().size(); j++){
+                FingerTable fingerTable = node.getFingerStarts().get(j);
+
+                assert fingerTable.getNode().equals(nodeList.get(i).get(j));
+                assert fingerTable.getStart().equals(starts.get(i).get(j));
+                assert fingerTable.getFrom().equals(intervals.get(i).get(j).get(0));
+                assert fingerTable.getTo().equals(intervals.get(i).get(j).get(1));
+            }
+
+        }
+
+        assert findSuccessor(2, 0, chordNodes, 3).equals(3);
+        for(int i = 4; i < 8; i++){
+            assert findSuccessor(i, 0, chordNodes, 3).equals(0);
+        }
+
     }
 }
